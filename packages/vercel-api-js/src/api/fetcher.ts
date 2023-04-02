b86e4f06d0d6ae47ff9df6599248b2596b1f2266
Alexis Rico <sferadev@gmail.com>
2023-04-02 10:54:17 +0200

    Update types
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- packages/vercel-api-js/src/api/fetcher.ts
+++ packages/vercel-api-js/src/api/fetcher.ts
@@ -1,22 +1,22 @@
-import { FetchImpl } from "../utils/fetch";
+import { FetchImpl } from '../utils/fetch';
 
 export type FetcherExtraProps = {
   token: string;
   fetchImpl: FetchImpl;
 };
 
-const baseUrl = "https://api.vercel.com";
+const baseUrl = 'https://api.vercel.com';
 
-export type ErrorWrapper<TError> = TError | { status: "unknown"; payload: string };
+export type ErrorWrapper<TError> = TError | { status: 'unknown'; payload: string };
 
 export type FetcherOptions<TBody, THeaders, TQueryParams, TPathParams> = {
   url: string;
   method: string;
-  body?: TBody;
-  headers?: THeaders;
-  queryParams?: TQueryParams;
-  pathParams?: TPathParams;
-  signal?: AbortSignal;
+  body?: TBody | undefined;
+  headers?: THeaders | undefined;
+  queryParams?: TQueryParams | undefined;
+  pathParams?: TPathParams | undefined;
+  signal?: AbortSignal | undefined;
 } & FetcherExtraProps;
 
 export async function fetch<
@@ -35,13 +35,13 @@ export async function fetch<
   queryParams,
   signal,
   token,
-  fetchImpl,
+  fetchImpl
 }: FetcherOptions<TBody, THeaders, TQueryParams, TPathParams>): Promise<TData> {
   try {
     const requestHeaders: HeadersInit = {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`,
-      ...headers,
+      ...headers
     };
 
     /**
@@ -50,15 +50,15 @@ export async function fetch<
      * the correct boundary.
      * https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects#sending_files_using_a_formdata_object
      */
-    if (requestHeaders["Content-Type"].toLowerCase().includes("multipart/form-data")) {
-      delete requestHeaders["Content-Type"];
+    if (requestHeaders['Content-Type']?.toLowerCase().includes('multipart/form-data')) {
+      delete requestHeaders['Content-Type'];
     }
 
     const response = await fetchImpl(`${baseUrl}${resolveUrl(url, queryParams, pathParams)}`, {
       signal,
       method: method.toUpperCase(),
       body: body ? (body instanceof FormData ? body : JSON.stringify(body)) : undefined,
-      headers: requestHeaders,
+      headers: requestHeaders
     });
     if (!response.ok) {
       let error: ErrorWrapper<TError>;
@@ -66,15 +66,15 @@ export async function fetch<
         error = await response.json();
       } catch (e) {
         error = {
-          status: "unknown" as const,
-          payload: e instanceof Error ? `Unexpected error (${e.message})` : "Unexpected error",
+          status: 'unknown' as const,
+          payload: e instanceof Error ? `Unexpected error (${e.message})` : 'Unexpected error'
         };
       }
 
       throw error;
     }
 
-    if (response.headers?.get("content-type")?.includes("json")) {
+    if (response.headers?.get('content-type')?.includes('json')) {
       return await response.json();
     } else {
       // if it is not a json response, assume it is a blob and cast it to TData
@@ -82,9 +82,9 @@ export async function fetch<
     }
   } catch (e) {
     let errorObject: Error = {
-      name: "unknown" as const,
-      message: e instanceof Error ? `Network error (${e.message})` : "Network error",
-      stack: e as string,
+      name: 'unknown' as const,
+      message: e instanceof Error ? `Network error (${e.message})` : 'Network error',
+      stack: e as string
     };
     throw errorObject;
   }
@@ -93,5 +93,5 @@ export async function fetch<
 const resolveUrl = (url: string, queryParams: Record<string, string> = {}, pathParams: Record<string, string> = {}) => {
   let query = new URLSearchParams(queryParams).toString();
   if (query) query = `?${query}`;
-  return url.replace(/\{\w*\}/g, (key) => pathParams[key.slice(1, -1)]) + query;
+  return url.replace(/\{\w*\}/g, (key) => pathParams[key.slice(1, -1)] ?? '') + query;
 };
--- packages/vercel-api-js/src/utils/fetch.ts
+++ packages/vercel-api-js/src/utils/fetch.ts
@@ -1,15 +1,22 @@
-export type RequestInit = { body?: string | FormData; headers?: Record<string, string>; method?: string; signal?: any };
+export type RequestInit = {
+  body?: string | FormData | undefined;
+  headers?: Record<string, string> | undefined;
+  method?: string | undefined;
+  signal?: any | undefined;
+};
 
 export type Response = {
-    ok: boolean;
-    status: number;
-    url: string;
-    json(): Promise<any>;
-    text(): Promise<string>;
-    headers?: {
+  ok: boolean;
+  status: number;
+  url: string;
+  json(): Promise<any>;
+  text(): Promise<string>;
+  headers?:
+    | {
         get(name: string): string | null;
-    };
+      }
+    | undefined;
 };
 
 // Typed only the subset of the spec we actually use (to be able to build a simple mock)
-export type FetchImpl = (url: string, init?: RequestInit) => Promise<Response>;
+export type FetchImpl = (url: string, init?: RequestInit | undefined) => Promise<Response>;
