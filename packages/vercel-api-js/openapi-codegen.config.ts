e4ff74570bb23ea91130e430b991cc4068a8fc37
Alexis Rico <sferadev@gmail.com>
2023-04-04 16:36:41 +0200

    Add `request` method with string literals

--- packages/vercel-api-js/openapi-codegen.config.ts
+++ packages/vercel-api-js/openapi-codegen.config.ts
@@ -1,48 +1,90 @@
-import { defineConfig } from "@openapi-codegen/cli";
-import { Context } from "@openapi-codegen/cli/lib/types";
-import { generateFetchers, generateSchemaTypes } from "@openapi-codegen/typescript";
+import { defineConfig } from '@openapi-codegen/cli';
+import { Context } from '@openapi-codegen/cli/lib/types';
+import { generateFetchers, generateSchemaTypes } from '@openapi-codegen/typescript';
+import { Project, VariableDeclarationKind } from 'ts-morph';
+import ts from 'typescript';
 
 export default defineConfig({
-    vercel: {
-        from: {
-            source: "url",
-            url: "https://openapi.vercel.sh",
-        },
-        outputDir: "src/api",
-        to: async (context) => {
-            const filenamePrefix = "";
-
-            // Rename invalid component name for searchRepo
-            context.openAPIDocument = updateMethod({
-                openAPIDocument: context.openAPIDocument,
-                path: "/v1/integrations/search-repo",
-                method: "get",
-                update: (operation) => ({ ...operation, operationId: "searchRepo" }),
-            });
-
-            const { schemasFiles } = await generateSchemaTypes(context, { filenamePrefix });
-            await generateFetchers(context, { filenamePrefix, schemasFiles });
-        },
+  vercel: {
+    from: {
+      source: 'url',
+      url: 'https://openapi.vercel.sh'
     },
+    outputDir: 'src/api',
+    to: async (context) => {
+      const filenamePrefix = '';
+
+      // Rename invalid component name for searchRepo
+      context.openAPIDocument = updateMethod({
+        openAPIDocument: context.openAPIDocument,
+        path: '/v1/integrations/search-repo',
+        method: 'get',
+        update: (operation) => ({ ...operation, operationId: 'searchRepo' })
+      });
+
+      const { schemasFiles } = await generateSchemaTypes(context, { filenamePrefix });
+      await generateFetchers(context, { filenamePrefix, schemasFiles });
+      await context.writeFile('extra.ts', buildExtraFile(context));
+    }
+  }
 });
 
 function updateMethod({
-    openAPIDocument,
-    path,
-    method,
-    update,
+  openAPIDocument,
+  path,
+  method,
+  update
 }: {
-    openAPIDocument: Context["openAPIDocument"];
-    path: string;
-    method: string;
-    update: (
-        operation: Context["openAPIDocument"]["paths"][string][string]
-    ) => Context["openAPIDocument"]["paths"][string][string];
+  openAPIDocument: Context['openAPIDocument'];
+  path: string;
+  method: string;
+  update: (
+    operation: Context['openAPIDocument']['paths'][string][string]
+  ) => Context['openAPIDocument']['paths'][string][string];
 }) {
-    const operation = openAPIDocument.paths?.[path]?.[method];
-    if (!operation) return openAPIDocument;
+  const operation = openAPIDocument.paths?.[path]?.[method];
+  if (!operation) return openAPIDocument;
 
-    openAPIDocument.paths[path][method] = { ...operation, ...update(operation) };
+  openAPIDocument.paths[path][method] = { ...operation, ...update(operation) };
 
-    return openAPIDocument;
+  return openAPIDocument;
+}
+
+function buildExtraFile(context: Context) {
+  const project = new Project({
+    useInMemoryFileSystem: true,
+    compilerOptions: { module: ts.ModuleKind.ESNext, target: ts.ScriptTarget['ES2020'] }
+  });
+
+  const sourceFile = project.createSourceFile('extra.ts');
+
+  const operationsByPath = Object.fromEntries(
+    Object.entries(context.openAPIDocument.paths ?? {}).flatMap(([path, methods]) => {
+      return Object.entries(methods)
+        .filter(([method]) => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'].includes(method.toUpperCase()))
+        .map(([method, operation]: [string, any]) => [`${method.toUpperCase()} ${path}`, operation.operationId]);
+    })
+  );
+
+  sourceFile.addImportDeclaration({
+    namedImports: Object.values(operationsByPath),
+    moduleSpecifier: './components'
+  });
+
+  sourceFile.addVariableStatement({
+    isExported: true,
+    declarationKind: VariableDeclarationKind.Const,
+    declarations: [
+      {
+        name: 'operationsByPath',
+        initializer: `{
+            ${Object.entries(operationsByPath)
+              .map(([path, operation]) => `"${path}": ${operation}`)
+              .join(',\n')}
+        }`
+      }
+    ]
+  });
+
+  return sourceFile.getFullText();
 }
