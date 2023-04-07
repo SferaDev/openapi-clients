a07cc558a6b01a0bbbba25c499df0487410a1b41
Alexis Rico <sferadev@gmail.com>
2023-04-07 13:09:15 +0200

    Delete packages/dhis2-api directory

--- packages/dhis2-api/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# dhis2-api
-
-## 0.1.0
-
-### Minor Changes
-
-- 4461c03: First release
--- packages/dhis2-api/src/api/v40/index.ts
+++
@@ -1,3 +0,0 @@
-import type * as Schemas from './schemas';
-import type * as Components from './components';
-export { Schemas, Components };
