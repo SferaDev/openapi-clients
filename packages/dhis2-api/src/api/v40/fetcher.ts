d2d624ed1a12d1663d60c3815bbd72fcb9e99aa1
Alexis Rico <sferadev@gmail.com>
2023-04-07 10:19:59 +0200

    Update components
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- packages/dhis2-api/src/api/v40/fetcher.ts
+++ packages/dhis2-api/src/api/v40/fetcher.ts
@@ -1,1 +1,10 @@
+import { Credentials } from '../../client';
+import { FetchImpl } from '../../utils/fetch';
+
+export type FetcherExtraProps = {
+  fetchImpl: FetchImpl;
+  baseUrl: string;
+  credentials: Credentials;
+};
+
 export * from '../fetcher';
