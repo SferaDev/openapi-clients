38a6b43e383d04320979fe64f69a87fd450f9ac0
SferaDev <SferaDev@users.noreply.github.com>
2023-04-17 21:26:19 +0000

    Update OpenAPI Spec

--- packages/vercel-api-js/src/api/schemas.ts
+++ packages/vercel-api-js/src/api/schemas.ts
@@ -753,32 +753,74 @@ export type TeamLimited = {
    */
   saml?: {
     /**
-     * From T, pick a set of properties whose keys are in the union K
+     * Information for the SAML Single Sign-On configuration.
      */
     connection?: {
-      createdAt?: number | null;
-      creator: string;
-      domain: string;
-      id: string;
-      name: string;
-      recordType: 'A' | 'AAAA' | 'ALIAS' | 'CAA' | 'CNAME' | 'MX' | 'SRV' | 'TXT' | 'NS';
-      ttl?: number;
-      type: 'record' | 'record-sys';
-      value: string;
+      /**
+       * The Identity Provider "type", for example Okta.
+       *
+       * @example OktaSAML
+       */
+      type: string;
+      /**
+       * Current status of the connection.
+       *
+       * @example linked
+       */
+      status: string;
+      /**
+       * Current state of the connection.
+       *
+       * @example active
+       */
+      state: string;
+      /**
+       * Timestamp (in milliseconds) of when the configuration was connected.
+       *
+       * @example 1611796915677
+       */
+      connectedAt: number;
+      /**
+       * Timestamp (in milliseconds) of when the last webhook event was received from WorkOS.
+       *
+       * @example 1611796915677
+       */
+      lastReceivedWebhookEvent?: number;
     };
     /**
-     * From T, pick a set of properties whose keys are in the union K
+     * Information for the SAML Single Sign-On configuration.
      */
     directory?: {
-      createdAt?: number | null;
-      creator: string;
-      domain: string;
-      id: string;
-      name: string;
-      recordType: 'A' | 'AAAA' | 'ALIAS' | 'CAA' | 'CNAME' | 'MX' | 'SRV' | 'TXT' | 'NS';
-      ttl?: number;
-      type: 'record' | 'record-sys';
-      value: string;
+      /**
+       * The Identity Provider "type", for example Okta.
+       *
+       * @example OktaSAML
+       */
+      type: string;
+      /**
+       * Current status of the connection.
+       *
+       * @example linked
+       */
+      status: string;
+      /**
+       * Current state of the connection.
+       *
+       * @example active
+       */
+      state: string;
+      /**
+       * Timestamp (in milliseconds) of when the configuration was connected.
+       *
+       * @example 1611796915677
+       */
+      connectedAt: number;
+      /**
+       * Timestamp (in milliseconds) of when the last webhook event was received from WorkOS.
+       *
+       * @example 1611796915677
+       */
+      lastReceivedWebhookEvent?: number;
     };
     /**
      * When `true`, interactions with the Team **must** be done with an authentication token that has been authenticated with the Team's SAML Single Sign-On provider.
