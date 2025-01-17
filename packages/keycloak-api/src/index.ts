ae275d04e5e5ef4bcc806040237ac008a8b27117
Alexis Rico <sferadev@gmail.com>
2025-01-17 10:12:11 +0100

    Export extra (#643)
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- packages/keycloak-api/src/index.ts
+++ packages/keycloak-api/src/index.ts
@@ -2,8 +2,10 @@ export * from "./client";
 
 import type * as AdminSchemas from "./admin/schemas";
 import type * as AdminComponents from "./admin/components";
+import * as AdminExtra from "./admin/extra";
 
 import type * as AccountSchemas from "./account/schemas";
 import type * as AccountComponents from "./account/components";
+import * as AccountExtra from "./account/extra";
 
-export { AdminSchemas, AdminComponents, AccountSchemas, AccountComponents };
+export { AdminSchemas, AdminComponents, AccountSchemas, AccountComponents, AdminExtra, AccountExtra };
