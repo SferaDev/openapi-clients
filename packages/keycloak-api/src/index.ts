966c3e2e99c78d80527539597302727331432304
Alexis Rico <sferadev@gmail.com>
2025-01-17 09:32:40 +0100

    Add account api
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- packages/keycloak-api/src/index.ts
+++ packages/keycloak-api/src/index.ts
@@ -1,5 +1,9 @@
 export * from "./client";
 
-import type * as Schemas from "./admin/schemas";
-import type * as Components from "./admin/components";
-export { Schemas, Components };
+import type * as AdminSchemas from "./admin/schemas";
+import type * as AdminComponents from "./admin/components";
+
+import type * as AccountSchemas from "./account/schemas";
+import type * as AccountComponents from "./account/components";
+
+export { AdminSchemas, AdminComponents, AccountSchemas, AccountComponents };
